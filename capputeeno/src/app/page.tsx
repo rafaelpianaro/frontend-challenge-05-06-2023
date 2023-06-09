0b40e71d35bae5da79e3aeef9e792a5479f53271
Rafael Pianaro <rafaelpianaro@gmail.com>
2023-06-08 22:57:45 -0300

    refactor: extract providers

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -3,18 +3,13 @@
 import { FilterBar } from '@/components/filter-bar'
 import styles from './page.module.css'
 import { ProductsList } from '@/components/products-list'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Home() {
 
-  const client = new QueryClient();
-
   return (
-    <QueryClientProvider client={client}>
       <main className={styles.main}>
         <FilterBar />
         <ProductsList />
       </main>
-    </QueryClientProvider>
   )
 }
--- capputeeno/src/hooks/useLocalStorage.tsx
+++ capputeeno/src/hooks/useLocalStorage.tsx
@@ -5,9 +5,13 @@ export function useLocalStorage<T>(item: string, initialValue: T){
     const [value, setValue] = useState<T>(initialValue)
 
     useEffect(() => {
+
         if (typeof window === 'undefined') return;
+
         let value = localStorage.getItem(item)
+
         if(value) setValue(JSON.parse(value))
+        
     }, [window])
 
     const updateLocalStorage = (newValue: T) => {
